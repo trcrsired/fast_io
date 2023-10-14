98d0ea98bfe9dbf071c1725a4bcea82d906e9b16
MacroModel <liyn_xlfd@163.com>
2023-10-15 00:29:56 +0800

    fix basic_black_hole

--- include/fast_io_core_impl/black_hole.h
+++ include/fast_io_core_impl/black_hole.h
@@ -6,23 +6,36 @@ namespace fast_io
 //black_hole is a helper class which helps you remove device requirement for iobuf or iotransform.
 //sha256 for example. You do not need a real device. You want to send all your bits to black hole
 
-template<std::integral ch_type>
+template<::std::integral ch_type>
 struct basic_black_hole
 {
-public:
 	using char_type = ch_type;
+	using input_char_type = char_type;
+	using output_char_type = char_type;
 };
 
-template<std::integral ch_type,::std::contiguous_iterator Iter>
-inline constexpr void write(basic_black_hole<ch_type>,Iter,Iter){}
+template<::std::integral ch_type>
+inline void read_all_underflow_define(basic_black_hole<ch_type> iob, ch_type* first, ch_type* last) {}
 
-template<std::integral ch_type,::std::contiguous_iterator Iter>
-inline constexpr Iter read(basic_black_hole<ch_type>,Iter b,Iter){return b;}
+template<::std::integral ch_type>
+inline void write_all_overflow_define(basic_black_hole<ch_type> iob, ch_type const* first, ch_type const* last) {}
+
+template<::std::integral ch_type>
+inline void read_all_bytes_underflow_define(basic_black_hole<ch_type> iob, ::std::byte* first, ::std::byte* last) {}
+
+template<::std::integral ch_type>
+inline void write_all_bytes_overflow_define(basic_black_hole<ch_type> iob, ::std::byte const* first, ::std::byte const* last) {}
+
+template<::std::integral ch_type>
+inline constexpr basic_black_hole<ch_type> io_stream_ref_define(basic_black_hole<ch_type> other) noexcept
+{
+	return other;
+}
 
 template<std::integral ch_type>
-inline constexpr basic_black_hole<ch_type> io_value_handle(basic_black_hole<ch_type> h) noexcept
+inline constexpr basic_black_hole<ch_type> io_bytes_stream_ref_define(basic_black_hole<ch_type> other) noexcept
 {
-	return h;
+	return other;
 }
 
 }
