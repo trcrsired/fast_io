72b1b3cdac056289e0c7d7ad138feb21018655b2
MacroModel <liyn_xlfd@163.com>
2023-10-15 17:58:03 +0800

    update blackhole

--- include/fast_io_core_impl/black_hole.h
+++ include/fast_io_core_impl/black_hole.h
@@ -14,17 +14,35 @@ struct basic_black_hole
 	using output_char_type = char_type;
 };
 
-template<::std::integral ch_type>
-inline void read_all_underflow_define(basic_black_hole<ch_type> iob, ch_type* first, ch_type* last) {}
+using black_hole = basic_black_hole<char>;
+using wblack_hole = basic_black_hole<wchar_t>;
+using u8black_hole = basic_black_hole<char8_t>;
+using u16black_hole = basic_black_hole<char16_t>;
+using u32black_hole = basic_black_hole<char32_t>;
 
 template<::std::integral ch_type>
-inline void write_all_overflow_define(basic_black_hole<ch_type> iob, ch_type const* first, ch_type const* last) {}
+inline constexpr void read_all_underflow_define(basic_black_hole<ch_type> iob, ch_type* first, ch_type* last) noexcept {}
 
 template<::std::integral ch_type>
-inline void read_all_bytes_underflow_define(basic_black_hole<ch_type> iob, ::std::byte* first, ::std::byte* last) {}
+inline constexpr void write_all_overflow_define(basic_black_hole<ch_type> iob, ch_type const* first, ch_type const* last) noexcept {}
 
 template<::std::integral ch_type>
-inline void write_all_bytes_overflow_define(basic_black_hole<ch_type> iob, ::std::byte const* first, ::std::byte const* last) {}
+inline constexpr void read_all_bytes_underflow_define(basic_black_hole<ch_type> iob, ::std::byte* first, ::std::byte* last) noexcept {}
+
+template<::std::integral ch_type>
+inline constexpr void write_all_bytes_overflow_define(basic_black_hole<ch_type> iob, ::std::byte const* first, ::std::byte const* last) noexcept {}
+
+template<::std::integral ch_type>
+inline constexpr void pread_all_underflow_define(basic_black_hole<ch_type> iob, ch_type* first, ch_type* last) noexcept {}
+
+template<::std::integral ch_type>
+inline constexpr void pwrite_all_overflow_define(basic_black_hole<ch_type> iob, ch_type const* first, ch_type const* last) noexcept {}
+
+template<::std::integral ch_type>
+inline constexpr void pread_all_bytes_underflow_define(basic_black_hole<ch_type> iob, ::std::byte* first, ::std::byte* last) noexcept {}
+
+template<::std::integral ch_type>
+inline constexpr void pwrite_all_bytes_overflow_define(basic_black_hole<ch_type> iob, ::std::byte const* first, ::std::byte const* last) noexcept {}
 
 template<::std::integral ch_type>
 inline constexpr basic_black_hole<ch_type> io_stream_ref_define(basic_black_hole<ch_type> other) noexcept
@@ -32,10 +50,19 @@ inline constexpr basic_black_hole<ch_type> io_stream_ref_define(basic_black_hole
 	return other;
 }
 
-template<std::integral ch_type>
+template<::std::integral ch_type>
 inline constexpr basic_black_hole<ch_type> io_bytes_stream_ref_define(basic_black_hole<ch_type> other) noexcept
 {
 	return other;
 }
+
+template<::std::integral ch_type>
+inline constexpr void io_stream_buffer_flush_define(basic_black_hole<ch_type> other) noexcept {}
+
+template<typename ch_type>
+inline constexpr ::fast_io::intfpos_t io_stream_seek_define(basic_black_hole<ch_type> fb, ::fast_io::intfpos_t off, ::fast_io::seekdir sdir) noexcept {}
+
+template<typename ch_type>
+inline constexpr ::fast_io::intfpos_t io_stream_seek_bytes_define(basic_black_hole<ch_type> fb, ::fast_io::intfpos_t off, ::fast_io::seekdir sdir) noexcept {}
 
 }
