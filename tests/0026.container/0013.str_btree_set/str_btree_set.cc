71e41fe8cbe3f466abbb457047a72284ec122d70
trcrsired <uwgghhbcad@gmail.com>
2025-06-17 00:11:36 +0800

    set

--- tests/0026.container/0013.str_btree_set/str_btree_set.cc
+++ tests/0026.container/0013.str_btree_set/str_btree_set.cc
@@ -34,10 +34,32 @@ inline constexpr void str_btree_set_inorder_traverse(
 
 int main()
 {
-	::fast_io::containers::basic_str_btree_set<char, ::fast_io::native_global_allocator> bset;
-	bset.insert_key("asfasf");
-	bset.insert_key("asfasfsf");
-	bset.insert_key("aaa");
+	::fast_io::containers::basic_str_btree_set<char, ::fast_io::native_global_allocator, 16> bset;
+    bset.insert_key("apple");
+    bset.insert_key("banana");
+    bset.insert_key("cherry");
+    bset.insert_key("date");
+    bset.insert_key("elderberry");
+    bset.insert_key("fig");
+    bset.insert_key("grape");
+    bset.insert_key("honeydew");
+    bset.insert_key("kiwi");
+    bset.insert_key("lemon");
+    bset.insert_key("mango");
+    bset.insert_key("nectarine");
+    bset.insert_key("orange");
+    bset.insert_key("papaya");
+    bset.insert_key("quince");
+    bset.insert_key("raspberry");
+    bset.insert_key("strawberry");
+    bset.insert_key("tangerine");
+    bset.insert_key("ugli");
+    bset.insert_key("vanilla");
+    bset.insert_key("watermelon");
+    bset.insert_key("xigua");
+    bset.insert_key("yellowfruit");
+    bset.insert_key("zucchini");
+
 	str_btree_set_inorder_traverse(bset.root, [](::fast_io::string_view v) {
 		::fast_io::io::println(v);
 	});
