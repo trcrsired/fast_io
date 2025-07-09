1329622ce998d0b466ae3fc6efa2f09a8f8ab09e
trcrsired <uwgghhbcad@gmail.com>
2025-07-09 16:00:53 +0800

    [btree] add fuzzing to debug the code

---
+++ fuzzing/0007.containers/btree/str_btree_set.cc
@@ -0,0 +1,18 @@
+#include <fast_io.h>
+#include <fast_io_dsal/impl/associative_string.h>
+#include <fast_io_dsal/impl/str_btree_set.h>
+#include <fast_io_dsal/string_view.h>
+
+thread_local ::fast_io::containers::basic_str_btree_set<char, ::fast_io::native_global_allocator, 4> bset;
+thread_local ::std::size_t n{};
+
+extern "C" int LLVMFuzzerTestOneInput(::std::uint8_t *data, ::std::size_t size)
+{
+    if(n==100000)
+    {
+        bset.clear_destroy();
+    }
+    bset.insert_key(::fast_io::string_view(reinterpret_cast<char const*>(data),size));
+    ++n;
+    return 0;
+}
