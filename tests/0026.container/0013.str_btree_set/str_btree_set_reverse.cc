2eeda2fb2a54f44f60bd1bf128e1a06493d26a6f
trcrsired <uwgghhbcad@gmail.com>
2025-07-12 01:55:47 +0800

    clang-format for str_btree_set_reverse.cc

--- tests/0026.container/0013.str_btree_set/str_btree_set_reverse.cc
+++ tests/0026.container/0013.str_btree_set/str_btree_set_reverse.cc
@@ -35,35 +35,33 @@ inline constexpr void str_btree_set_inorder_traverse(
 int main()
 {
 	::fast_io::containers::basic_str_btree_set<char, ::fast_io::native_global_allocator, 4> bset;
-	constexpr
-		::fast_io::string_view fruits[]{
-    ::fast_io::string_view("zucchini"),
-    ::fast_io::string_view("yellowfruit"),
-    ::fast_io::string_view("xigua"),
-    ::fast_io::string_view("watermelon"),
-    ::fast_io::string_view("vanilla"),
-    ::fast_io::string_view("ugli"),
-    ::fast_io::string_view("tangerine"),
-    ::fast_io::string_view("strawberry"),
-    ::fast_io::string_view("raspberry"),
-    ::fast_io::string_view("quince"),
-    ::fast_io::string_view("papaya"),
-    ::fast_io::string_view("orange"),
-    ::fast_io::string_view("nectarine"),
-    ::fast_io::string_view("mango"),
-    ::fast_io::string_view("lemon"),
-    ::fast_io::string_view("kiwi"),
-    ::fast_io::string_view("grape"),
-    ::fast_io::string_view("fig"),
-    ::fast_io::string_view("elderberry"),
-    ::fast_io::string_view("date"),
-    ::fast_io::string_view("cherry"),
-    ::fast_io::string_view("banana"),
-    ::fast_io::string_view("apple"),
-    ::fast_io::string_view("honeydew")
-};
+	constexpr ::fast_io::string_view fruits[]{
+		::fast_io::string_view("zucchini"),
+		::fast_io::string_view("yellowfruit"),
+		::fast_io::string_view("xigua"),
+		::fast_io::string_view("watermelon"),
+		::fast_io::string_view("vanilla"),
+		::fast_io::string_view("ugli"),
+		::fast_io::string_view("tangerine"),
+		::fast_io::string_view("strawberry"),
+		::fast_io::string_view("raspberry"),
+		::fast_io::string_view("quince"),
+		::fast_io::string_view("papaya"),
+		::fast_io::string_view("orange"),
+		::fast_io::string_view("nectarine"),
+		::fast_io::string_view("mango"),
+		::fast_io::string_view("lemon"),
+		::fast_io::string_view("kiwi"),
+		::fast_io::string_view("grape"),
+		::fast_io::string_view("fig"),
+		::fast_io::string_view("elderberry"),
+		::fast_io::string_view("date"),
+		::fast_io::string_view("cherry"),
+		::fast_io::string_view("banana"),
+		::fast_io::string_view("apple"),
+		::fast_io::string_view("honeydew")};
 
-	for(auto const & e : fruits)
+	for (auto const &e : fruits)
 	{
 		bset.insert_key(e);
 	}
