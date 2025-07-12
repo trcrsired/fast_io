3a4ccc655e49fa6ba92893b084d1f7df0bbdbb25
trcrsired <uwgghhbcad@gmail.com>
2025-07-12 19:32:55 +0800

    [str_btree_set] add ::std::unordered_set bench

---
+++ benchmark/0011.containers/str_btree_set/std_unordered_set.cc
@@ -0,0 +1,30 @@
+#include <unordered_set>
+#include <string>
+#include <fast_io.h>
+#include <fast_io_driver/timer.h>
+#include "gentest.h"
+
+int main()
+{
+	auto vec{::gentest<std::string>()};
+	::fast_io::timer t(u8"str_btree_set");
+	::std::unordered_set<::std::string> bset;
+	{
+		::fast_io::timer t(u8"insert");
+		for (auto const &e : vec)
+		{
+			bset.insert(e);
+		}
+	}
+	{
+		::std::size_t count{};
+		{
+			::fast_io::timer t(u8"contains");
+			for (auto const &e : vec)
+			{
+				count += static_cast<::std::size_t>(bset.contains(e));
+			}
+		}
+		::fast_io::io::perrln("count=", count);
+	}
+}
