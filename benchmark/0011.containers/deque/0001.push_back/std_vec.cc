a7e3b2858b615fb06ef858f901ef60bfd42673eb
trcrsired <uwgghhbcad@gmail.com>
2026-05-15 02:55:32 +0800

    [skip ci][deque] benchmark needs std_vec one

---
+++ benchmark/0011.containers/deque/0001.push_back/std_vec.cc
@@ -0,0 +1,26 @@
+﻿#include <fast_io.h>
+#include <fast_io_driver/timer.h>
+#include <vector>
+
+int main()
+{
+	fast_io::timer tm(u8"std::vector");
+	std::vector<std::size_t> vec;
+	constexpr std::size_t n{100000000};
+	{
+		fast_io::timer tm1(u8"push_back");
+		for (std::size_t i{}; i != n; ++i)
+		{
+			vec.push_back(i);
+		}
+	}
+	::std::size_t sum{};
+	{
+		fast_io::timer tm1(u8"loop");
+		for (auto const e : vec)
+		{
+			sum += e;
+		}
+	}
+	::fast_io::io::perrln("sum=", sum);
+}
