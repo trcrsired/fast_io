bc01fa1b6daf4a65104bc67ce8ec7a4fe5b15d65
trcrsired <uwgghhbcad@gmail.com>
2026-07-08 12:30:51 +0800

    [xxh3] add tests for xxh3 so i can go and test it although these tests need tweaks

---
+++ fuzzing/0000.write/xxh3_64bits_fio.cc
@@ -0,0 +1,13 @@
+#include <fast_io.h>
+#include <fast_io_dsal/str_swiss_set.h>
+
+thread_local fast_io::u32obuf_file obf(u8"/dev/null");
+
+using namespace fast_io::io;
+
+extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *ptr, std::size_t n) noexcept
+{
+	::std::uint_least64_t h{::fast_io::details::xxh3_64bits_fio(ptr, n)};
+	println(obf, h);
+	return 0;
+}
