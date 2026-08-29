f68309f11a371dab9f27cedfabe370085aa27201
trcrsired <uwgghhbcad@gmail.com>
2026-08-30 00:14:31 +0800

    [herbceptions] enable throws_wrapper macro since i have resolved the
    issue in my compiler

--- include/fast_io_core_impl/herbceptions/impl.h
+++ include/fast_io_core_impl/herbceptions/impl.h
@@ -1,6 +1,5 @@
 #pragma once
 
 #if defined(__HERBCEPTIONS__) && __has_include(<herbceptions/error>)
-#include <herbceptions/error>
 #include "output.h"
 #endif
--- include/fast_io_core_impl/herbceptions/throws_wrapper.h
+++ include/fast_io_core_impl/herbceptions/throws_wrapper.h
@@ -3,71 +3,71 @@
 namespace fast_io::herbceptions
 {
 
-template<typename T>
-requires ::std::is_enum_v<T>
+template <typename T>
+	requires ::std::is_enum_v<T>
 inline constexpr void throws_errc_generic([[maybe_unused]] T ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-#if defined(__HERBCEPTIONS__) && 0
-    throw throws ec;
+#if defined(__HERBCEPTIONS__)
+	throw throws ec;
 #else
-    ::fast_io::fast_terminate();
+	::fast_io::fast_terminate();
 #endif
 }
 
 inline constexpr void throws_errc([[maybe_unused]] ::std::errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(ec);
+	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 
 inline constexpr void throws_errc_with_value([[maybe_unused]] int ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(static_cast<::std::errc>(ec));
+	::fast_io::herbceptions::throws_errc_generic(static_cast<::std::errc>(ec));
 }
 
 inline constexpr void throws_parse_errc([[maybe_unused]] ::fast_io::freestanding::parse_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::parse_errc>(ec));
+	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::parse_errc>(ec));
 }
 
 #if defined(_WIN32) || defined(__CYGWIN__)
 inline constexpr void throws_nt_errc([[maybe_unused]] ::fast_io::freestanding::nt_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(ec);
+	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 inline constexpr void throws_nt_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::nt_errc>(ec));
+	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::nt_errc>(ec));
 }
 
 inline constexpr void throws_win32_errc([[maybe_unused]] ::fast_io::freestanding::win32_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(ec);
+	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 
 inline constexpr void throws_win32_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::win32_errc>(ec));
+	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::win32_errc>(ec));
 }
 
 inline constexpr void throws_com_errc([[maybe_unused]] ::fast_io::freestanding::com_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(ec);
+	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 
 inline constexpr void throws_com_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::com_errc>(ec));
+	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::com_errc>(ec));
 }
 
 inline constexpr void throws_wine_errc([[maybe_unused]] ::fast_io::freestanding::wine_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(ec);
+	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 
 inline constexpr void throws_wine_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
-    ::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::wine_errc>(ec));
+	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::wine_errc>(ec));
 }
 
 #endif
-}
+} // namespace fast_io::herbceptions
