4d8fd944787f0b7da98476125b8166b149719d99
trcrsired <uwgghhbcad@gmail.com>
2026-09-02 05:12:50 +0800

    win32_code.h should add [[noreturn]] attribute

--- include/fast_io_hosted/platforms/win32_code.h
+++ include/fast_io_hosted/platforms/win32_code.h
@@ -3,12 +3,12 @@
 namespace fast_io
 {
 
-inline void throw_win32_error() FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline void throw_win32_error() FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_win32_errc_with_value(::fast_io::win32::GetLastError());
 }
 
-inline void throw_win32_error([[maybe_unused]] ::std::uint_least32_t err) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline void throw_win32_error([[maybe_unused]] ::std::uint_least32_t err) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_win32_errc_with_value(err);
 }
