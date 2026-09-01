fd5c4b40b66d7d8e9c4fd360be59b2f36d1b36e4
trcrsired <uwgghhbcad@gmail.com>
2026-09-02 05:08:20 +0800

    [herbceptions] add [[noreturn]] to all throws_wrapper functions to avoid warnings

--- include/fast_io_core_impl/herbceptions/throws_wrapper.h
+++ include/fast_io_core_impl/herbceptions/throws_wrapper.h
@@ -14,7 +14,7 @@ template <typename T>
 #endif
 }
 
-inline constexpr void throws_errc([[maybe_unused]] ::std::errc ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_errc([[maybe_unused]] ::std::errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(ec);
 }
@@ -24,47 +24,47 @@ inline constexpr void throws_errc([[maybe_unused]] ::std::errc ec) FAST_IO_HERBC
 	::fast_io::herbceptions::throws_errc_generic(static_cast<::std::errc>(ec));
 }
 
-inline constexpr void throws_parse_errc([[maybe_unused]] ::fast_io::freestanding::parse_errc ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_parse_errc([[maybe_unused]] ::fast_io::freestanding::parse_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::parse_errc>(ec));
 }
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-inline constexpr void throws_nt_errc([[maybe_unused]] ::fast_io::freestanding::nt_errc ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_nt_errc([[maybe_unused]] ::fast_io::freestanding::nt_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(ec);
 }
-inline constexpr void throws_nt_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_nt_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::nt_errc>(ec));
 }
 
-inline constexpr void throws_win32_errc([[maybe_unused]] ::fast_io::freestanding::win32_errc ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_win32_errc([[maybe_unused]] ::fast_io::freestanding::win32_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 
-inline constexpr void throws_win32_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_win32_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::win32_errc>(ec));
 }
 
-inline constexpr void throws_com_errc([[maybe_unused]] ::fast_io::freestanding::com_errc ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_com_errc([[maybe_unused]] ::fast_io::freestanding::com_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 
-inline constexpr void throws_com_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_com_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::com_errc>(ec));
 }
 
-inline constexpr void throws_wine_errc([[maybe_unused]] ::fast_io::freestanding::wine_errc ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_wine_errc([[maybe_unused]] ::fast_io::freestanding::wine_errc ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(ec);
 }
 
-inline constexpr void throws_wine_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
+[[noreturn]] inline constexpr void throws_wine_errc_with_value([[maybe_unused]] ::std::uint_least32_t ec) FAST_IO_HERBCEPTIONS_THROWS
 {
 	::fast_io::herbceptions::throws_errc_generic(static_cast<::fast_io::freestanding::wine_errc>(ec));
 }
