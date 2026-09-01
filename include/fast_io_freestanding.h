15e9e88f461f9e56ae0553e3561f9bd5a923b7c1
trcrsired <oyzawqgcfc@gmail.com>
2026-09-01 21:09:37 +0800

    remove decorators.
    Decorators were designed poorly and should be removed long time ago
    
    and now it is called transcoder, not decorator. just remove all of them

--- include/fast_io_freestanding.h
+++ include/fast_io_freestanding.h
@@ -21,8 +21,6 @@
 #include "fast_io_unit/floating/impl.h"
 #endif
 #include "fast_io_freestanding_impl/io_buffer/impl.h"
-#include "fast_io_freestanding_impl/io_deco_filter/impl.h"
-#include "fast_io_freestanding_impl/decorators/impl.h"
 #include "fast_io_freestanding_impl/auto_indent.h"
 #include "fast_io_freestanding_impl/serializations/impl.h"
 #include "fast_io_freestanding_impl/space_reserve.h"
