d6e8a82fa3a055061ee5eff97e651d9d1357a764
trcrsired <oyzawqgcfc@gmail.com>
2026-09-22 13:06:38 +0800

    [benchmark] add str_btree_map to strmap

---
+++ benchmark/0011.containers/strmap/str_btree_map.cc
@@ -0,0 +1,56 @@
+#include <ranges>
+#include <fast_io_dsal/str_btree_map.h>
+#include <fast_io.h>
+#include <fast_io_driver/timer.h>
+#include "gentest.h"
+
+int main()
+{
+	auto vec{::gentest()};
+	::fast_io::timer t(u8"str_btree_map");
+	::fast_io::str_btree_map<::std::size_t> bmap;
+	{
+		::fast_io::timer t(u8"insert_key");
+		for (::std::size_t i{}; i != vec.size(); ++i)
+		{
+			bmap.insert_key(::fast_io::string_view{vec[i].data(), vec[i].size()}, i);
+		}
+	}
+	{
+		::std::size_t count{};
+		{
+			::fast_io::timer t(u8"contains");
+			for (auto const &e : vec)
+			{
+				count += static_cast<::std::size_t>(bmap.contains(::fast_io::string_view{e.data(), e.size()}));
+			}
+		}
+		::fast_io::io::perrln("count=", count);
+	}
+	{
+		::std::size_t total_size{};
+		::std::size_t total_mapped{};
+		{
+			::fast_io::timer t(u8"iteration");
+			for (auto const &e : bmap)
+			{
+				total_size += e.key().size();
+				total_mapped += e.mapped();
+			}
+		}
+		::fast_io::io::perrln("total_size=", total_size, " total_mapped=", total_mapped);
+	}
+	{
+		::std::size_t total_size{};
+		::std::size_t total_mapped{};
+		{
+			::fast_io::timer t(u8"reverse iteration");
+			for (auto const &e : ::std::ranges::reverse_view(bmap))
+			{
+				total_size += e.key().size();
+				total_mapped += e.mapped();
+			}
+		}
+		::fast_io::io::perrln("total_size=", total_size, " total_mapped=", total_mapped);
+	}
+}
