0e5be693673b1fd998f8226b739109038484ab2f
trcrsired <uwgghhbcad@gmail.com>
2026-09-24 06:54:38 +0800

    [a+b]herb

---
+++ examples/0002.a+b/apb_herb.cc
@@ -0,0 +1,19 @@
+﻿#include <fast_io.h>
+
+using namespace fast_io::iomnp;
+
+int main()
+try
+{
+	std::size_t a, b;
+#if 0
+	print("Please input two integers\n");
+#endif
+	scan(a, b);
+	println(a + b);
+}
+catch throws(std::error e)
+{
+	perrln(e);
+	return 1;
+}
